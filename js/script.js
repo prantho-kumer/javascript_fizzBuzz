a6fcb73e964ecd331f9c7f5a68813871047b4c9d
Prantho Kumer <57418052+prantho-kumer@users.noreply.github.com>
2021-02-23 19:52:23 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var output = [];
+var count = 1; 
+
+function fizzBuzz(){
+
+while (count <=100){
+
+    if( count % 3 === 0 && count % 5 === 0){
+        output.push("fizzBuzz");
+    }
+
+    else if( count % 3 === 0){
+         output.push("fizz");
+    }else if( count % 5 === 0){
+        output.push("buzz");
+    }
+    else{
+         output.push(count);
+    }
+
+    count++;
+}
+   
+ return output;
+}
+document.getElementById("output").innerHTML = fizzBuzz();
